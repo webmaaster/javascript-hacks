0a4b630d9d06e5f8cbe4136ad7fe4683229e1d14
Software Engineer <softwareengineer@LambdaSr233sMBP.home>
2020-09-30 18:11:45 -0400

    created class

---
+++ module-001/test.js
@@ -0,0 +1,36 @@
+console.log("Hello everyone!");
+
+
+/* Test starts....
+
+console.log("This is testing")
+alert("It's fantastic!")
+
+alert("Let's rock the Javascript!")
+
+Testing ends */
+
+//Variables?
+// What is variable?
+// Variables are just like a container that holds data
+
+/* Example of variables ...
+
+var a = "Python Programming";
+console.log(a);
+alert(a);
+alert('I love '+ a + " "+ "very much.");
+
+Ends example */
+var a = "Python Programming";
+var someNumber = 47;
+console.log(someNumber);
+
+alert("I am "+ someNumber + " year old, and I love "+ a);
+
+//Dom 
+document.getElementById("myID").innerHTML = "Hello There, what's up?";
+
+var age = prompt('What is your age?');
+
+alert(age); 
